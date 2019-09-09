66415152755de80a2af9b6f3ec9d99d8ad6105dc
lihe <lihe1@weshare.com.cn>
2019-09-09 11:53:28 +0800

    day13 尾递归优化

--- app/src/main/java/com/example/rivenlee/kotlin_learn_diary/day04/Day04.kt
+++ app/src/main/java/com/example/rivenlee/kotlin_learn_diary/day04/Day04.kt
@@ -7,27 +7,27 @@ package com.example.rivenlee.kotlin_learn_diary.day04
  */
 
 fun main(args: Array<String>) {
-    for(arg in args){
+    for (arg in args) {
         println(arg)
     }
 
-    for((index, value) in args.withIndex()){
+    for ((index, value) in args.withIndex()) {
         println("$index -> $value")
     }
 
-    for(indexedValue in args.withIndex()){
+    for (indexedValue in args.withIndex()) {
         println("${indexedValue.index} -> ${indexedValue.value}")
     }
 
 
-    val array = intArrayOf(1,3,5,7)
+    val array = intArrayOf(1, 3, 5, 7)
     // *array表示将当前数组展示，仅支持数组，不支持集合
     // string = "hello", 可变长参数之后的具名参数
     hello(3.0, *array, string = "hello")
 }
 
 
-fun hello(double: Double, vararg ints: Int, string: String){
-    ints.forEach (::println)
+fun hello(double: Double, vararg ints: Int, string: String) {
+    ints.forEach(::println)
     println(string)
 }
--- app/src/main/java/com/example/rivenlee/kotlin_learn_diary/day14/Main.kt
+++ app/src/main/java/com/example/rivenlee/kotlin_learn_diary/day14/Main.kt
@@ -5,3 +5,63 @@ package com.example.rivenlee.kotlin_learn_diary.day14
  * date: 2019/9/6
  * email: rivenlee0@gmail.com
  */
+
+val string = "HelloWorld"
+
+fun makeFun(): () -> Unit {
+
+    var count = 0
+    return fun() {
+        println(++count)
+    }
+}
+
+fun fibonacci(): () -> Long {
+    var first = 0L
+    var second = 1L
+    return fun(): Long {
+        val result = second
+        second += first
+        first = second - first
+        return result
+    }
+}
+
+fun fibonacci2(): Iterable<Long> {
+    var first = 0L
+    var second = 1L
+    return Iterable {
+        object : LongIterator() {
+            override fun hasNext() = true
+
+            override fun nextLong(): Long {
+                val result = second
+                second += first
+                first = second - first
+                return result
+            }
+        }
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val x = fibonacci()
+    for (index in 1 until 11) {
+        println(x())
+    }
+
+    for (index in fibonacci2()) {
+        if (index > 100) break
+        println(x())
+    }
+}
+
+fun add(x :Int) = fun(y :Int) = x + y
+
+fun add1(x: Int): (Int) -> Int{
+    return fun (y: Int):Int{
+        return x + y
+    }
+}
+

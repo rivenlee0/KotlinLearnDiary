5e9bdb4a4ca698acbd13dd2761d0e67a545cd64f
rivenlee <rivenlee0@gmail.com>
2021-01-27 16:19:19 +0800

    kotlin 函数引用

--- app/src/main/java/com/example/rivenlee/kotlin_learn_diary/project/activity/ProgressActivity.kt
+++ app/src/main/java/com/example/rivenlee/kotlin_learn_diary/project/activity/ProgressActivity.kt
@@ -3,12 +3,40 @@ package com.example.rivenlee.kotlin_learn_diary.project.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rivenlee.kotlin_learn_diary.R
+import kotlinx.android.synthetic.main.activity_progress.*
+import org.jetbrains.anko.toast
 
 class ProgressActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_progress)
+
+        tv_wrapper.setOnClickListener {
+            doWrapper()
+        }
+    }
+
+    fun funa(func: (msg: String)-> Unit){
+
+        fun b(){
+            toast("this is funb")
+            func("this is func")
+        }
+        return b()
+    }
+
+
+    private fun func(msg: String){
+        toast("this is fun -> $msg")
+    }
+
+    private val valc = fun(msg: String) {
+        toast("this is val -> $msg")
+    }
+
+    private fun doWrapper(){
+        funa(::func)
     }
 
 }
